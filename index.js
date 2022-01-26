95e185c5183c4b28c4229cc2ad43dd6e111afab4
mdnajmul <mdnajmulovi@gmail.com>
2022-01-27 02:57:22 +0600

    create api for get/fetch all blogs with pagination

--- index.js
+++ index.js
@@ -10,18 +10,57 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Database Info
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3u7yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+//live server link: https://mighty-savannah-90389.herokuapp.com/
 
 async function run() {
   try {
     await client.connect();
 
     console.log("database connected successfully");
+
+    //database name
+    const database = client.db("travel_expo_blog");
+
+    // Collections
+    const blogCollection = database.collection("blogs");
+    const userCollection = database.collection("users");
+    const topSpotCollection = database.collection("top_spots");
+
+    /* ========================= Blog Collection START ======================= */
+
+    // GET - Get all travel blogs with pagination
+    app.get("/blogs", async (req, res) => {
+      const query = { status: "approved" };
+      const cursor = blogCollection.find(query);
+
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+
+      const count = await cursor.count();
+
+      let blogs;
+
+      if (count > 0) {
+        if (page) {
+          blogs = await cursor
+            .skip(page * size)
+            .limit(size)
+            .toArray();
+        } else {
+          blogs = await cursor.toArray();
+        }
+        res.json(blogs);
+      } else {
+        res.json({ message: "Blog Not Found!" });
+      }
+    });
   } finally {
     // await client.close();
   }
